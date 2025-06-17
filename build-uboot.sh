213a706f39562ec3b41a54c45756c08d9254d462
EatPrilosec <ae3nerdgod@gmail.com>
2025-06-17 01:19:46 -0400

    Update build-uboot.sh

--- build-uboot.sh
+++ build-uboot.sh
@@ -39,8 +39,8 @@ then
     echo grabbing toolchain
     echo
     mkdir -p /opt/toolchains
-    [[ ! -f "gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz" ]] && wget https://releases.linaro.org/components/toolchain/binaries/6.3-2017.05/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -Ogcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
-    tar Jxvf gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -C /opt/toolchains/
+    [[ ! -f "gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz" ]] && wget -q https://releases.linaro.org/components/toolchain/binaries/6.3-2017.05/aarch64-linux-gnu/gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -Ogcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
+    tar Jxf gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz -C /opt/toolchains/
     #rm gcc-linaro-6.3.1-2017.05-x86_64_aarch64-linux-gnu.tar.xz
 fi
 
